48240c39e496fd625f8846e09629d49acdebc940
svitlana-kyrychenko <102665740+svitlana-kyrychenko@users.noreply.github.com>
2022-04-27 19:50:19 +0300

    Update run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -2,4 +2,4 @@ docker network create my-cassandra-network
 docker run --name cassandra-node1 --network my-cassandra-network -d cassandra:latest
 docker run --name cassandra-node2 --network my-cassandra-network -d -e CASSANDRA_SEEDS=cassandra-node1 cassandra:latest
 docker run --name cassandra-node3 --network my-cassandra-network -d -e CASSANDRA_SEEDS=cassandra-node1 cassandra:latest
-docker run -p 8081:80 -it --network my-cassandra-network --rm cassandra cqlsh cassandra-node1
+docker run -it --network my-cassandra-network --rm cassandra cqlsh cassandra-node1
